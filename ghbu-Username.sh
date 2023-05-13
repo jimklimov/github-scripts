b267b6c3938326facf93c95f802f1c2fc12ae9fa
Jim Klimov <jimklimov@gmail.com>
2023-05-13 16:29:55 +0200

    ghbu-Username.sh: for a weekly backup schedule, bump GHBU_PRUNE_AFTER_N_DAYS=30

--- ghbu-Username.sh
+++ ghbu-Username.sh
@@ -3,7 +3,10 @@
 # Sample wrapper to call backup-github.sh for org/user/gists
 # (C) 2023 by Jim Klimov
 
+# Regarding pruning (if enabled): the systemd unit default is to
+# run the backup weekly; this way 30 days (below) keep 4-5 copies.
 #GHBU_PRUNE_OLD="false" \
+GHBU_PRUNE_AFTER_N_DAYS=30 \
 GHBU_ORGMODE="user" \
 GHBU_ORG="Username" \
 GHBU_UNAME="Username" \
